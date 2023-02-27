0fbcb6ab17d1f278f5f20f930c51a617073641b8
JuicL <kolomin111@gmail.com>
2023-02-27 22:46:12 +0400

    init

--- extension.js
+++ extension.js
@@ -1,10 +1,29 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require("fs/promises");
+const path = require("path");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-
+async function getNewestDirectory(dirName) {
+	let dirs = await fs.readdir(dirName, {
+	  withFileTypes: true,
+	});
+	dirs = dirs.filter((file) => file.isDirectory()).map((dirent) => dirent.name);
+	let newestFolder = await fs.stat(path.join(dirName, dirs[0]));
+	let newestFolderName = dirs[0];
+	for (let directory of dirs) {
+	  const stats = await fs.stat(path.join(dirName, directory));
+	  if (stats.ctimeMs > newestFolder.ctimeMs) {
+		newestFolder = stats;
+		newestFolderName = directory;
+	  }
+	}
+	//console.log("newestFolder", newestFolder);
+	//console.log("newestFolderName", newestFolderName);
+	return path.resolve(path.join(dirName, newestFolderName));
+  }
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -18,34 +37,52 @@ function activate(context) {
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	
+	var projectPath;
+	var outPath;
 	let disposable = vscode.commands.registerCommand('cppmetrics.helloWorld', function () {
 		//const os = require("os");
 		//let osVersion = os.platform();
 		//console.log("OS Version:", osVersion);
 		
 		// Текущая папка
-		var projectPath;
-		var outPath;
 
 		if(vscode.workspace.workspaceFolders !== undefined) {
 			projectPath = vscode.workspace.workspaceFolders[0].uri.fsPath ; 
 		} 
 		else {
-			let message = "YOUR-EXTENSION: Working folder not found, open a folder an try again" ;
+			let message = "Working folder not found, open a folder an try again" ;
 			vscode.window.showErrorMessage(message);
+			return;
 		}
 		
-		var cppMetricCorePath = "C:/Users/User/source/repos/AntlrTreeVisualization/AntlrTreeVisualization/bin/Debug";
+		var cppMetricCorePath = "C:\\Users/User/source/repos/AntlrTreeVisualization/AntlrTreeVisualization/bin/Debug";
 		const exec = require('child_process').exec;
-		exec(`cd ${cppMetricCorePath}`
-		+ `& start AntlrTreeVisualization.exe`
-		+`-i ${projectPath}`
-		+`-o ${outPath}`
-		, { encoding: 'utf-8' });
+		try {
+			exec(`cd ${cppMetricCorePath}`
+			+ `& start AntlrTreeVisualization.exe`
+			+`-i ${projectPath}`
+			+`-o ${outPath}`
+			, { encoding: 'utf-8' });
+		} catch (error) {
+			vscode.window.showInformationMessage('Error!');
+			return;
+		}
 		
-
-
-		vscode.window.showInformationMessage('Hello World from CPPMetrics!');
+		vscode.window.showInformationMessage('Reports done!','View Report').then(selection =>{
+			if(selection === 'View Report')
+			{
+				let outPutChannel = vscode.window.createOutputChannel("Cpp-metrics","Cpp-metrics-id");
+				var folder = getNewestDirectory(projectPath);
+				folder.then(function(res){
+					console.log("newestFolderName", res);
+					//let fileName = path.join(res, "index.html")
+					const exec = require('child_process').exec;
+					exec(`cd ${res} & start index.html`, { encoding: 'utf-8' });
+				});
+				outPutChannel.appendLine(folder);
+			}
+			//console.log(selection);
+		});
 	});
 
 	context.subscriptions.push(disposable);
